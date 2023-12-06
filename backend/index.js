836a9ab4ad2945701d3a93c8836c5437bafbc34c
EliasGithub <etsegay@miu.edu>
2023-12-06 09:41:58 -0500

    model created

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,18 @@
-import express from "express"
-
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config()
+import cors from "cors"
 const app=express();
-app.listen(3000,()=>{
-    console.log("server is runing on port 3000")
+const {PORT,MONGODB_URL}=process.env
+//middlewares
+app.use(express.json())
+app.use(cors())
+//connect to mongodb
+mongoose.connect(MONGODB_URL)
+.then(()=>{app.listen(PORT,()=>{
+    console.log(`server is runing on port ${PORT}!`)
+})})
+.catch((error)=>{
+    console.log(error)
 })
